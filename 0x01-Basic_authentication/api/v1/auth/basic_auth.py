1b0f66ad242b1b7d8028a0c95746f76d184b0450
mrxdoffical <berogergesg@gmail.com>
2024-11-12 23:20:15 +0200

    task9

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -33,7 +33,9 @@ class BasicAuth(Auth):
         except Exception:
             return None
 
-    def extract_user_credentials(self, decoded_base64_authorization_header: str) -> (str, str):
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header:
+                                     str) -> (str, str):
         """extracting user data
         """
         if decoded_base64_authorization_header is None:
