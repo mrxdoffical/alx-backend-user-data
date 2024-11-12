132e153c6ae8ec6b02cddbb366da3aa6ab6c6503
mrxdoffical <berogergesg@gmail.com>
2024-11-12 22:03:44 +0200

    task5

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,11 +7,17 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+from api.v1.auth.auth import Auth
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+auth_type = getenv("AUTH_TYPE")
+
+if auth_type == "auth":
+    auth = Auth()
 
 
 @app.errorhandler(404)
@@ -47,6 +53,23 @@ def forbidden_function():
     """forbidden url
     """
     abort(403)
+
+
+@app.before_request
+def before_request_func():
+    """Filtering each request
+    """
+    if auth is None:
+        return
+    excluded_path = ['/api/v1/status/',
+                     '/api/v1/unauthorized/',
+                     '/api/v1/forbidden/']
+    if not auth.require_auth(request.path, excluded_path):
+        return
+    if auth.authorization_header(request) is None:
+        abort(401)
+    if auth.current_user(request):
+        abort(403)
 
 
 if __name__ == "__main__":
