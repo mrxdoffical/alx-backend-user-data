216e189c05dd5aff3918ecc805d27596dfc153cf
mrxdoffical <berogergesg@gmail.com>
2024-11-16 23:23:01 +0200

    task7

--- 0x02-Session_authentication/api/v1/views/__init__.py
+++ 0x02-Session_authentication/api/v1/views/__init__.py
@@ -7,6 +7,13 @@ from api.v1.views.users import *
 from api.v1.views.session_auth import *
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+"""
+Initialize views module
+======================
+
+This module initializes the views module which contains the routes of
+the API
+"""
 
 
 User.load_from_file()
