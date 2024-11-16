7620386adbac43308338ac7b3e858cdda5cae0b5
mrxdoffical <berogergesg@gmail.com>
2024-11-16 23:13:22 +0200

    task3

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -39,6 +39,6 @@ class Auth:
         """Returns a cookie value from a request"""
         if request is None:
             return None
-        
+
         session_name = getenv('SESSION_NAME', '_my_session_id')
         return request.cookies.get(session_name)
