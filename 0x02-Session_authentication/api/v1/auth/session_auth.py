3a128a3b09f440de99ec81ef4de33818dd44e7bc
mrxdoffical <berogergesg@gmail.com>
2024-11-16 23:10:31 +0200

    task2

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,7 +2,6 @@
 """Session authentication module
 """
 from api.v1.auth.auth import Auth
-from models.user import User
 import uuid
 
 
@@ -20,44 +19,3 @@ class SessionAuth(Auth):
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
-
-    def user_id_for_session_id(self, session_id: str = None) -> str:
-        """Returns a User ID based on a Session ID
-        """
-        if session_id is None or not isinstance(session_id, str):
-            return None
-
-        return self.user_id_by_session_id.get(session_id)
-
-    def current_user(self, request=None):
-        """Returns a User instance based on a cookie value
-        """
-        if request is None:
-            return None
-
-        session_id = self.session_cookie(request)
-        if session_id is None:
-            return None
-
-        user_id = self.user_id_for_session_id(session_id)
-        if user_id is None:
-            return None
-
-        return User.get(user_id)
-
-    def destroy_session(self, request=None):
-        """Deletes the user session / logout
-        """
-        if request is None:
-            return False
-
-        session_id = self.session_cookie(request)
-        if session_id is None:
-            return False
-
-        user_id = self.user_id_for_session_id(session_id)
-        if user_id is None:
-            return False
-
-        del self.user_id_by_session_id[session_id]
-        return True
